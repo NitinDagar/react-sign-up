07950620d3d3d4b4f5ab0bf295598d55fc9dd15b
nitindagar <nitindagar7@gmail.com>
2020-08-14 19:03:24 +0530

    Updates

---
+++ precache-manifest.d02bd6cbd7520f86566b9864b569ceed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258d8f0545a98a9e7221443e6e76b4fd",
+    "url": "/react-sign-up/index.html"
+  },
+  {
+    "revision": "13cfee2c21ffb512b56e",
+    "url": "/react-sign-up/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ff0667d5c9139766f0ff",
+    "url": "/react-sign-up/static/css/main.08a3d22d.chunk.css"
+  },
+  {
+    "revision": "13cfee2c21ffb512b56e",
+    "url": "/react-sign-up/static/js/2.b05d1e09.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-sign-up/static/js/2.b05d1e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0667d5c9139766f0ff",
+    "url": "/react-sign-up/static/js/main.549a816e.chunk.js"
+  },
+  {
+    "revision": "a5acdea6307524e8facb",
+    "url": "/react-sign-up/static/js/runtime-main.7af4f99e.js"
+  }
+]);
